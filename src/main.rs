2a15809986fa9957bd1c97e77ec5a0210effeb43
kazu914 <nkz0914ssk@gmail.com>
2022-06-02 22:17:52 +0900

    add clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[clap(subcommand)]
+    action: Action,
+}
+
+#[derive(Subcommand, Debug)]
+enum Action {
+    Add { snipet: String },
+    List {},
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Args::parse();
+    match cli.action {
+        Action::Add { snipet } => {
+            println!("INPUT: {}", snipet)
+        }
+        Action::List {} => {
+            print!("List here")
+        }
+    }
 }
