8b7e954fde00bc6895f6a253082fc7101c78c9e0
kazu914 <nkz0914ssk@gmail.com>
2022-07-31 15:25:22 +0900

    (feat) add data_dir option

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,33 @@
-use std::{fs, path::PathBuf};
+use std::{
+    fs,
+    path::{Path, PathBuf},
+    process,
+};
 
 use clap::{Parser, Subcommand};
 use directories::ProjectDirs;
 use scrawl::error;
+
+const PKG_NAME: &str = env!("CARGO_PKG_NAME");
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
     #[clap(subcommand)]
     action: Action,
+
+    /// <Optional> Specify the directory to store kakisute files
+    #[clap(long = "data_dir")]
+    data_dir: Option<String>,
 }
 
 #[derive(Subcommand, Debug)]
 enum Action {
-    New { filename: Option<String> },
+    /// Create new kakisute
+    New {
+        /// <Optional> Specify file name
+        filename: Option<String>,
+    },
     List {},
 }
 
@@ -22,23 +36,55 @@ struct DataDir {
 }
 
 impl DataDir {
-    fn setup() -> Self {
-        let project_dirs = ProjectDirs::from("", "", "kakisute").unwrap();
-        let _ = fs::create_dir_all(project_dirs.data_dir());
+    fn setup(dir: Option<String>) -> Self {
+        let data_dir_path = if let Some(dir) = dir {
+            Path::new(&dir).to_owned()
+        } else {
+            let project_dirs = ProjectDirs::from("", "", PKG_NAME).unwrap();
+            project_dirs.data_dir().to_path_buf()
+        };
+
+        Self::create_dir(&data_dir_path);
+        Self::check_readonly(&data_dir_path);
+
         DataDir {
-            path: project_dirs.data_dir().to_path_buf(),
+            path: data_dir_path.to_path_buf(),
         }
     }
 
     fn join(&self, filename: &str) -> PathBuf {
         self.path.join(filename)
     }
+
+    fn create_dir(path: &PathBuf) {
+        fs::create_dir_all(&path).unwrap_or_else(|err| {
+            eprintln!("Error: Can't make data directory: {:?}", path);
+            eprintln!("{:?}", err);
+            process::exit(1)
+        });
+    }
+
+    fn check_readonly(path: &PathBuf) {
+        let metadata = path.metadata();
+        if let Ok(metadata) = metadata {
+            if metadata.permissions().readonly() {
+                eprintln!("Error: Directory {:?} is READONLY", path);
+                process::exit(1)
+            }
+        } else {
+            eprintln!(
+                "Unexpected Error: Can not get permissions information: {:?}",
+                path
+            );
+            process::exit(1)
+        }
+    }
 }
 
 fn main() -> Result<(), error::ScrawlError> {
     let cli = Args::parse();
 
-    let data_dir = DataDir::setup();
+    let data_dir = DataDir::setup(cli.data_dir);
 
     match cli.action {
         Action::New { filename } => {
