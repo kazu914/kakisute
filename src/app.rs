6cb8d9f2a6ccb6cc5cac866aac5f169e75791246
kazu914 <nkz0914ssk@gmail.com>
2022-08-07 16:29:01 +0900

    (feat) have kakisute_list in app

--- src/app.rs
+++ src/app.rs
@@ -6,14 +6,16 @@ use crate::{data_dir::DataDir, kakisute_list::KakisuteList};
 
 pub struct App {
     data_dir: DataDir,
-    kakisute_list: Option<KakisuteList>,
+    kakisute_list: KakisuteList,
 }
 
 impl App {
-    pub fn new(data_dir: Option<String>) -> Self {
+    pub fn new(data_dir_arg: Option<String>) -> Self {
+        let data_dir = DataDir::setup(data_dir_arg);
+        let kakisute_list = KakisuteList::from_dir(data_dir.read_dir());
         App {
-            data_dir: DataDir::setup(data_dir),
-            kakisute_list: None,
+            data_dir,
+            kakisute_list,
         }
     }
 }
--- src/app/edit_service.rs
+++ src/app/edit_service.rs
@@ -1,17 +1,16 @@
-use crate::{editor, kakisute_list::KakisuteList};
+use crate::editor;
 
 use super::App;
 
 impl App {
     pub fn edit(&self, is_latest: bool, file_name: Option<String>) {
-        let kakisute_list = KakisuteList::from_dir(self.data_dir.read_dir());
         match file_name {
             Some(file_name) => {
-                self.edit_by_file_name(kakisute_list, file_name);
+                self.edit_by_file_name(file_name);
             }
             None => {
                 if is_latest {
-                    self.edit_latest(kakisute_list);
+                    self.edit_latest();
                 } else {
                     println!("edit expect a file name or \"--latest\" flag")
                 }
@@ -19,8 +18,8 @@ impl App {
         }
     }
 
-    fn edit_by_file_name(&self, kakisute_list: KakisuteList, file_name: String) {
-        let kakisute = kakisute_list.get_by_file_name(&file_name);
+    fn edit_by_file_name(&self, file_name: String) {
+        let kakisute = self.kakisute_list.get_by_file_name(&file_name);
         match kakisute {
             Some(kakisute) => {
                 editor::edit(&self.data_dir, &kakisute.file_name());
@@ -30,7 +29,7 @@ impl App {
             }
         }
     }
-    fn edit_latest(&self, kakisute_list: KakisuteList) {
-        editor::edit(&self.data_dir, &kakisute_list.get_latest().file_name());
+    fn edit_latest(&self) {
+        editor::edit(&self.data_dir, &self.kakisute_list.get_latest().file_name());
     }
 }
