c4c3252eb67b59bf9dd9f9e9e77b7894444a25e8
kazu914 <nkz0914ssk@gmail.com>
2022-08-14 18:00:41 +0900

    (refacter) add selector one

--- src/app/edit_service.rs
+++ src/app/edit_service.rs
@@ -4,32 +4,15 @@ use super::App;
 
 impl App {
     pub fn edit(&self, is_latest: bool, file_name: Option<String>) {
-        match file_name {
-            Some(file_name) => {
-                self.edit_by_file_name(file_name);
-            }
-            None => {
-                if is_latest {
-                    self.edit_latest();
-                } else {
-                    println!("edit expect a file name or \"--latest\" flag")
-                }
-            }
-        }
-    }
+        let kakisute = self.kakisute_list.single_select(is_latest, file_name);
 
-    fn edit_by_file_name(&self, file_name: String) {
-        let kakisute = self.kakisute_list.get_by_file_name(&file_name);
         match kakisute {
             Some(kakisute) => {
                 editor::edit(&self.data_dir, &kakisute.file_name());
             }
             None => {
-                println!("Can not find: {}", file_name);
+                println!("Can not find one matching the query");
             }
         }
-    }
-    fn edit_latest(&self) {
-        editor::edit(&self.data_dir, &self.kakisute_list.get_latest().file_name());
     }
 }
--- src/kakisute_list/selector.rs
+++ src/kakisute_list/selector.rs
@@ -3,11 +3,28 @@ use crate::kakisute_file::KakisuteFile;
 use super::KakisuteList;
 
 impl KakisuteList {
-    pub fn get_latest(&self) -> &KakisuteFile {
-        self.files.iter().max().unwrap()
+    pub fn single_select(
+        &self,
+        is_latest: bool,
+        file_name: Option<String>,
+    ) -> Option<&KakisuteFile> {
+        match file_name {
+            Some(file_name) => self.get_by_file_name(&file_name),
+            None => {
+                if is_latest {
+                    self.get_latest()
+                } else {
+                    println!("edit expect a file name or \"--latest\" flag");
+                    None
+                }
+            }
+        }
+    }
+    fn get_latest(&self) -> Option<&KakisuteFile> {
+        self.files.iter().max()
     }
 
-    pub fn get_by_file_name(&self, file_name: &str) -> Option<&KakisuteFile> {
+    fn get_by_file_name(&self, file_name: &str) -> Option<&KakisuteFile> {
         self.files.iter().find(|file| file.file_name() == file_name)
     }
 }
