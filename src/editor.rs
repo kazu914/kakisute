fb84e5a52ccaa6f8f1a72dfedc8ab4abe344fd33
kazu914 <nkz0914ssk@gmail.com>
2022-08-17 21:44:33 +0900

    (fmt) apply clippy --fix

--- src/editor.rs
+++ src/editor.rs
@@ -2,5 +2,5 @@ use crate::data_dir::DataDir;
 
 pub fn edit(data_dir: &DataDir, file_name: &str) {
     let file_path = data_dir.join(file_name);
-    let _output = scrawl::edit(file_path).unwrap();
+    scrawl::edit(file_path).unwrap();
 }
