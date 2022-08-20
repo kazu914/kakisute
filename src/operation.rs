3b399addcd3c8c3118835437da5c0f4c6464637c
kazu914 <nkz0914ssk@gmail.com>
2022-08-20 17:01:17 +0900

    (refactor) move show command

--- src/operation.rs
+++ src/operation.rs
@@ -1,6 +1,16 @@
+use std::process::Command;
+
 use crate::data_dir::DataDir;
 
 pub fn edit(data_dir: &DataDir, file_name: &str) {
     let file_path = data_dir.join(file_name);
     scrawl::edit(file_path).unwrap();
+}
+
+pub fn show(data_dir: &DataDir, file_name: &str) {
+    let file_path = data_dir.join(file_name);
+    Command::new("cat")
+        .arg(file_path)
+        .spawn()
+        .expect("Not found cat command");
 }
