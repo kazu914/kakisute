76168e480c2ed32e8f2d7559ad509a77d821162d
kazu914 <nkz0914ssk@gmail.com>
2022-08-20 16:58:20 +0900

    (refactor) rename editor

--- src/app/new_service.rs
+++ src/app/new_service.rs
@@ -1,10 +1,10 @@
-use crate::{editor, kakisute_file::KakisuteFile};
+use crate::{operation, kakisute_file::KakisuteFile};
 
 use super::App;
 
 impl App {
     pub fn create_kakisute(&self, file_name: Option<String>) {
         let kakisute_file = KakisuteFile::new(file_name);
-        editor::edit(&self.data_dir, kakisute_file.file_name());
+        operation::edit(&self.data_dir, kakisute_file.file_name());
     }
 }
