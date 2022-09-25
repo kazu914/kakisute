21d20c80ef05dd47b8e38a92eef30c626abb4211
kazu914 <nkz0914ssk@gmail.com>
2022-09-25 18:08:59 +0900

    (refact) separate handler function

--- src/ui/index.rs
+++ src/ui/index.rs
@@ -28,72 +28,82 @@ fn render_loop(terminal_manager: &mut dyn TerminalManage, tui: &mut AppInteracto
         code, modifiers, ..
     }) = event::read()?
     {
-        match tui.get_mode() {
-            Mode::Insert => match (code, modifiers) {
-                (KeyCode::Esc, KeyModifiers::NONE) => {
-                    tui.clear_file_name();
-                    tui.enter_normal_mode();
-                }
-                (KeyCode::Char(c), KeyModifiers::NONE) => {
-                    tui.push_to_file_name(c);
-                }
-                (KeyCode::Backspace, KeyModifiers::NONE) => {
-                    tui.pop_from_file_name();
-                }
-                (KeyCode::Enter, KeyModifiers::NONE) => {
+        handle_input(code, modifiers, terminal_manager, tui)?;
+    };
+    Ok(())
+}
+
+fn handle_input(
+    key_code: KeyCode,
+    key_modifier: KeyModifiers,
+    terminal_manager: &mut dyn TerminalManage,
+    tui: &mut AppInteractor,
+) -> Result<()> {
+    match tui.get_mode() {
+        Mode::Insert => match (key_code, key_modifier) {
+            (KeyCode::Esc, KeyModifiers::NONE) => {
+                tui.clear_file_name();
+                tui.enter_normal_mode();
+            }
+            (KeyCode::Char(c), KeyModifiers::NONE) => {
+                tui.push_to_file_name(c);
+            }
+            (KeyCode::Backspace, KeyModifiers::NONE) => {
+                tui.pop_from_file_name();
+            }
+            (KeyCode::Enter, KeyModifiers::NONE) => {
+                terminal_manager.exit_app_screen()?;
+                tui.create_new_kakisute_with_file_name()?;
+                terminal_manager.enter_app_screen()?;
+                tui.reload();
+                tui.clear_file_name();
+            }
+            _ => {}
+        },
+        Mode::Normal => match (key_code, key_modifier) {
+            (KeyCode::Esc, KeyModifiers::NONE) | (KeyCode::Char('q'), KeyModifiers::NONE) => {
+                terminal_manager.exit_app_screen()?;
+                tui.exit();
+            }
+            (KeyCode::Char('N'), KeyModifiers::SHIFT) => {
+                tui.enter_insert_mode();
+            }
+            (KeyCode::Char('j'), KeyModifiers::NONE) => {
+                tui.select_next();
+            }
+            (KeyCode::Char('k'), KeyModifiers::NONE) => {
+                tui.select_previous();
+            }
+            (KeyCode::Char('e'), KeyModifiers::NONE) => {
+                if tui.is_kakisute_selected() {
                     terminal_manager.exit_app_screen()?;
-                    tui.create_new_kakisute_with_file_name()?;
-                    terminal_manager.enter_app_screen()?;
-                    tui.reload();
-                    tui.clear_file_name();
-                }
-                _ => {}
-            },
-            Mode::Normal => match (code, modifiers) {
-                (KeyCode::Esc, KeyModifiers::NONE) | (KeyCode::Char('q'), KeyModifiers::NONE) => {
-                    terminal_manager.exit_app_screen()?;
-                    tui.exit();
-                }
-                (KeyCode::Char('N'), KeyModifiers::SHIFT) => {
-                    tui.enter_insert_mode();
-                }
-                (KeyCode::Char('j'), KeyModifiers::NONE) => {
-                    tui.select_next();
-                }
-                (KeyCode::Char('k'), KeyModifiers::NONE) => {
-                    tui.select_previous();
-                }
-                (KeyCode::Char('e'), KeyModifiers::NONE) => {
-                    if tui.is_kakisute_selected() {
-                        terminal_manager.exit_app_screen()?;
-                        tui.edit_kakisute()?;
-                        terminal_manager.enter_app_screen()?;
-                        terminal_manager.clear_app_screen()?;
-                    }
-                }
-                (KeyCode::Char('n'), KeyModifiers::NONE) => {
-                    terminal_manager.exit_app_screen()?;
-                    tui.create_new_kakisute()?;
+                    tui.edit_kakisute()?;
                     terminal_manager.enter_app_screen()?;
                     terminal_manager.clear_app_screen()?;
-                    tui.reload();
                 }
-                (KeyCode::Char('d'), KeyModifiers::NONE) => {
-                    tui.enter_delete_mode();
-                }
-                _ => {}
-            },
-            Mode::DeleteConfirm => match (code, modifiers) {
-                (KeyCode::Esc, KeyModifiers::NONE) | (KeyCode::Char('n'), KeyModifiers::NONE) => {
-                    tui.enter_normal_mode();
-                }
-                (KeyCode::Char('Y'), KeyModifiers::SHIFT) => {
-                    tui.delete_kakisute()?;
-                    tui.reload();
-                }
-                _ => {}
-            },
-        }
-    };
+            }
+            (KeyCode::Char('n'), KeyModifiers::NONE) => {
+                terminal_manager.exit_app_screen()?;
+                tui.create_new_kakisute()?;
+                terminal_manager.enter_app_screen()?;
+                terminal_manager.clear_app_screen()?;
+                tui.reload();
+            }
+            (KeyCode::Char('d'), KeyModifiers::NONE) => {
+                tui.enter_delete_mode();
+            }
+            _ => {}
+        },
+        Mode::DeleteConfirm => match (key_code, key_modifier) {
+            (KeyCode::Esc, KeyModifiers::NONE) | (KeyCode::Char('n'), KeyModifiers::NONE) => {
+                tui.enter_normal_mode();
+            }
+            (KeyCode::Char('Y'), KeyModifiers::SHIFT) => {
+                tui.delete_kakisute()?;
+                tui.reload();
+            }
+            _ => {}
+        },
+    }
     Ok(())
 }
