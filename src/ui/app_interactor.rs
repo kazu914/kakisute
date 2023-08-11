c5bb209fa6e4fa83b847c4d9c927e98f0f9a4242
kazu914 <nkz0914ssk@gmail.com>
2023-08-11 23:26:28 +0900

    use RefCell for cache

--- src/ui/app_interactor.rs
+++ src/ui/app_interactor.rs
@@ -1,6 +1,7 @@
 use crate::service::ServiceTrait;
 use crate::ui::filtered_list::FilteredList;
 use anyhow::Result;
+use std::cell::RefCell;
 use std::collections::HashMap;
 use std::result::Result::Ok;
 
@@ -18,7 +19,7 @@ pub struct AppInteractor<'a> {
     exit: bool,
     service: &'a dyn ServiceTrait,
     filtered_list: FilteredList,
-    cached_content: HashMap<String, String>,
+    cached_content: RefCell<HashMap<String, String>>,
     kakisute_name_list: Vec<String>,
 }
 
@@ -33,7 +34,7 @@ impl<'a> AppInteractor<'a> {
             service,
             filtered_list,
             kakisute_name_list,
-            cached_content: HashMap::new(),
+            cached_content: RefCell::new(HashMap::new()),
         }
     }
 
@@ -48,7 +49,7 @@ impl<'a> AppInteractor<'a> {
         self.kakisute_name_list = self.service.get_kakisute_list();
         self.filtered_list = FilteredList::new(self.kakisute_name_list.len());
         self.mode = Mode::Normal;
-        self.cached_content = HashMap::new();
+        self.cached_content = RefCell::new(HashMap::new());
         Ok(())
     }
 
@@ -58,7 +59,7 @@ impl<'a> AppInteractor<'a> {
             &self.user_input,
             self.kakisute_name_list
                 .iter()
-                .map(|s| s.to_owned() + self.cached_content.get(s).unwrap().as_str())
+                .map(|s| s.to_owned() + self.cached_content.borrow().get(s).unwrap().as_str())
                 .collect::<Vec<String>>(),
         )
     }
@@ -84,26 +85,30 @@ impl<'a> AppInteractor<'a> {
         self.user_input.clear();
     }
 
-    pub fn get_selected_kakisute_content(&mut self) -> Option<String> {
+    pub fn get_selected_kakisute_content(&self) -> Option<String> {
         let index = self.filtered_list.get_original_index().ok()?;
         self.load_kakisute_content(index)
     }
 
-    fn load_kakisute_content(&mut self, index: usize) -> Option<String> {
-        // cached_content にあればそれを返す
-        let kakisute_name = self.kakisute_name_list.get(index)?;
-        let cached_content = self.cached_content.get(kakisute_name);
-        if let Some(content) = cached_content {
-            return Some(content.clone());
+    fn load_kakisute_content(&self, index: usize) -> Option<String> {
+        if let Some(content) = self.load_from_cache(index) {
+            return Some(content);
         }
 
-        // なければ service から取得して返す
         let content = self.service.get_content_by_index(index).ok();
+        let kakisute_name = self.kakisute_name_list.get(index)?;
         if let Some(content) = &content {
             self.cached_content
+                .borrow_mut()
                 .insert(kakisute_name.clone(), content.clone());
         }
         content
+    }
+
+    fn load_from_cache(&self, index: usize) -> Option<String> {
+        let kakisute_name = self.kakisute_name_list.get(index)?;
+        let binding = self.cached_content.borrow();
+        binding.get(kakisute_name).cloned()
     }
 
     pub fn edit_kakisute(&self) -> Result<String> {
@@ -214,7 +219,7 @@ speculate! {
     describe "empty app_interactor" {
         before {
             let mut service = ServiceMock::new(KakisuteList::new());
-            let mut app_interactor = AppInteractor::new(&mut service);
+            let  app_interactor = AppInteractor::new(&mut service);
         }
 
         it "return error if edit is called" {
